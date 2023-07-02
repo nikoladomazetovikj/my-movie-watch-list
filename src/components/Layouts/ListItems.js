119c7bccdf1e77a4e303d5eeb5e4d81a146c5477
nikoladomazetovikj <nikolad150@gmail.com>
2023-07-03 00:08:24 +0200

    wip: modal movie details on click

--- src/components/Layouts/ListItems.js
+++ src/components/Layouts/ListItems.js
@@ -1,57 +1,82 @@
-import {useState} from "react";
-import {Button, Modal} from 'react-bootstrap'
+import {useEffect, useState} from "react";
+import { Button, Modal } from "react-bootstrap";
 
-function ListItems({movies}) {
-
+function ListItems({ movies }) {
     const [showModal, setShowModal] = useState(false);
-    const [modalTarget, setModalTarget] = useState('');
+    const [modalTarget, setModalTarget] = useState("");
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://www.omdbapi.com/?i=${modalTarget}&apikey=${process.env.REACT_APP_API_KEY}`);
+                const data = await response.json();
+                setMovie(data);
+            } catch (error) {
+                console.error('Error fetching movies:', error);
+            }
+        };
+
+        fetchData();
+    }, [modalTarget]);
+
+    console.log(movie)
 
     const toggleModal = (target) => {
         setShowModal(!showModal);
         setModalTarget(target);
     };
 
-    return (
-       <>
-           <h3>Search Movies</h3>
-           {movies.map((movie) => (
-               <a
-                   key={movie.imdbID}
-                   className="list-group-item list-group-item-action"
-                   aria-current="true"
-                   data-bs-target={"#" + movie.imdbID}
-                   type='button'
-                   onClick={() => toggleModal(movie.imdbID)}
-               >
-                       <div className="row ">
-                           <div className="col-3">
-                               <img src={movie.Poster} alt={movie.Title} width="100" height="150" />
-                           </div>
-                           <div className="col-9 ">
-                               <h5 className="mb-1">{movie.Title}</h5>
-                               <p>Year: {movie.Year}</p>
-                               <p>Type: {movie.Type}</p>
-                           </div>
-                       </div>
-               </a>
-           ))}
+    const handleClose = () => setShowModal(false);
+    const handleShow = () => setShowModal(true);
 
-           <Modal show={showModal} >
-               <Modal.Header closeButton>
-                   <Modal.Title>Modal heading</Modal.Title>
-               </Modal.Header>
-               <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-               <Modal.Footer>
-                   <Button variant="secondary" >
-                       Close
-                   </Button>
-                   <Button variant="primary" >
-                       Save Changes
-                   </Button>
-               </Modal.Footer>
-           </Modal>
-       </>
+    return (
+        <>
+            <h3>Search Movies</h3>
+            {movies.map((movie) => (
+                <a
+                    key={movie.imdbID}
+                    className="list-group-item list-group-item-action"
+                    aria-current="true"
+                    data-bs-target={"#" + movie.imdbID}
+                    type="button"
+                    onClick={() => toggleModal(movie.imdbID)}
+                >
+                    <div className="row ">
+                        <div className="col-3">
+                            <img
+                                src={movie.Poster}
+                                alt={movie.Title}
+                                width="100"
+                                height="150"
+                            />
+                        </div>
+                        <div className="col-9 ">
+                            <h5 className="mb-1">{movie.Title}</h5>
+                            <p>Year: {movie.Year}</p>
+                            <p>Type: {movie.Type}</p>
+                        </div>
+                    </div>
+                </a>
+            ))}
+
+            <Modal show={showModal} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{movie.Title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{movie.Plot}</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Add to Watch List
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
 
-export default ListItems
+export default ListItems;
